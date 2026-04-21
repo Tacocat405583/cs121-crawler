3e9616d4a1142364892da9adc3c1fb30a4b3ecc8
unknown <nicolashernan2029@gmail.com>
2026-04-20 22:18:10 -0700

    Fixed format

--- scraper.py
+++ scraper.py
@@ -6,7 +6,19 @@ def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
-def extract_next_links(url, resp):
+def extract_next_links(url, resp)->list:
+
+    # Implementation required.
+    # url: the URL that was used to get the page
+    # resp.url: the actual url of the page
+    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.error: when status is not 200, you can check the error here, if needed.
+    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+    #         resp.raw_response.url: the url, again
+    #         resp.raw_response.content: the content of the page!
+    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+
+
 
     links = []
 
@@ -21,29 +33,9 @@ def extract_next_links(url, resp):
             absolute = urljoin(url, href)
             links.append(absolute)
 
-            
+
     return links
 
-
-    
-    
-
-
-
-    print(resp.url)
-    print(resp.status)
-    print(resp.raw_response.url)
-    print(resp.raw_response.content)
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
