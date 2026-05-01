b4b5ad889ddadb6c11999496a74793d6cb297f78
Linn Oo <lhoo@uci.edu>
2026-05-01 09:22:49 +0000

    feat: Created a modified checksum using partitions to individually sum parts of the document using XOR idx+1 and multiplying idx+1 for more positional awareness.

--- duplicate_detection.py
+++ duplicate_detection.py
@@ -8,3 +8,14 @@ FINGERPRINTS = {}
 N = 3
 NGRAM_MOD_VALUE = 4
 THRESHOLD = 0.9
+
+def partition_checksum(text: str, num_partitions: int = 2, width: int = 8) -> int:
+    text_partitions = [0] * num_partitions
+    for idx, char in enumerate(text):
+        text_partitions[idx % num_partitions] += (ord(char) ^ (idx + 1)) * (idx + 1)
+    
+    parsed_hash = ""
+    for i in range(num_partitions):
+        parsed_hash += f"{text_partitions[i]:0{width}d}"
+    
+    return int(parsed_hash)
