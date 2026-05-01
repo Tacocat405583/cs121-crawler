c57919e24c39d7072ae1288072920bab4308c10c
Linn Oo <lhoo@uci.edu>
2026-05-01 10:09:21 +0000

    bug: Removed redundancy concerning changing of parittions/width with each call. feat: Basing simhash bit size on the bit size of the # length generated by partitions * width.

--- duplicate_detection.py
+++ duplicate_detection.py
@@ -1,21 +1,33 @@
 from tokenizer import word_frequencies
 
+#partition checksum config
+NUM_PARTITIONS = 2
+WIDTH = 8
+
 # Exact duplicate
 SEEN_PAGES = set()
 
 # Near duplicate + config
+def bit_length(n: int) -> int:
+    length = 0
+    while n > 0:
+        n >>= 1
+        length += 1
+    return length
+
 FINGERPRINTS = {}
-BITS = 64 #simhash bits
+SIMHASH_BITS = bit_length(int("9" * (NUM_PARTITIONS * WIDTH)))
 THRESHOLD = 0.9
 
-def partition_checksum(text: str, num_partitions: int = 2, width: int = 8) -> int:
-    text_partitions = [0] * num_partitions
+
+def partition_checksum(text: str) -> int:
+    text_partitions = [0] * NUM_PARTITIONS
     for idx, char in enumerate(text):
-        text_partitions[idx % num_partitions] += (ord(char) ^ (idx + 1)) * (idx + 1)
+        text_partitions[idx % NUM_PARTITIONS] += (ord(char) ^ (idx + 1)) * (idx + 1)
     
     parsed_hash = ""
-    for i in range(num_partitions):
-        parsed_hash += f"{text_partitions[i]:0{width}d}"
+    for i in range(NUM_PARTITIONS):
+        parsed_hash += f"{text_partitions[i]:0{WIDTH}d}"
     
     return int(parsed_hash)
 
@@ -41,19 +53,22 @@ def is_exact_duplicate(text: str) -> bool:
 def simhash(text: str) -> int:
     # 1: process doc into a set of features with assoc weights
     word_weights = compute_word_frequencies(text)
-    vector = [0] * BITS
+    vector = [0] * SIMHASH_BITS
     for word, weight in word_weights.items():
         # generate a hash value for each word
         word_hash = partition_checksum(word)
             #update components by adding the weight for which corresponding bit
-            for i in range(BITS):
+            for i in range(SIMHASH_BITS):
                 if (word_hash >> i) & 1:
                     vector[i] += weight
                 else:
                     vector[i] -= weight
     # gen fingeprint by setting ith bit to 1 if pos else 0
     fingerprint = 0
-    for i in range(BITS):
+    for i in range(SIMHASH_BITS):
         if vector[i] > 0:
             fingerprint |= (1 << i)
     return fingerprint
+
+#simhash similarity 
+# s_(a,b) = sigma_n(hashA =)
