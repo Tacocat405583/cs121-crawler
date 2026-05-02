f6f65cedacbd2e95f9293eed9d6a49d76245c4be
Kyle Dang <kylekd1@uci.edu>
2026-05-01 17:35:02 -0700

    chore: remove leftover comments and fix formatting in scraper.py

--- scraper.py
+++ scraper.py
@@ -106,6 +106,7 @@ BLOCKED_SUBDOMAINS = {
     "helpdesk.ics.uci.edu",
 }
 
+
 def save_data():
     # Called automatically on exit (including Ctrl+C) via atexit
     with open("word_frequencies.json", "w") as f:
@@ -273,7 +274,7 @@ def is_valid(url):
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|pps|ppsx|doc|docx|xls|xlsx|names" #pptx added
+            + r"|ps|eps|tex|ppt|pptx|pps|ppsx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
